6bec3aaf3431aee9571a771c0ae20347a70c8235
Julián Swerdlin <julianswer36@gmail.com>
2021-06-16 18:55:35 -0300

    Update config.js

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,6 +1,9 @@
+import {config as dotenv} from 'dotenv';
+dotenv();
+
 export const config = {
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'tasksdb'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 }
